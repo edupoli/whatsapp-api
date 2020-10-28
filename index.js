9711e5808cb31e3728761db7f1c3a8149339d9b9
edupoli <eduardopolicarpo@gmail.com>
2020-10-28 11:21:55 -0300

    Atualizado projeto para nova Versao do Venom

--- index.js
+++ index.js
@@ -2,47 +2,42 @@ const express = require('express');
 const venom = require('venom-bot');
 const app = express();
 
-const parameters = {
-    headless: true, 
-    devtools: false,
-    useChrome: false,
-    debug: false,
-    logQR: true,
-    browserArgs: ['--no-sandbox'],
-    refreshQR: 15000,
-    autoClose: 60000,
-    disableSpins: true,
-  };
-  venom.create('sessao', async (base64Qr, asciiQR) => {
-      console.log('exportando qrcode');
-  }, (statusFind) =>{
-      console.log(statusFind)
-  }, parameters).then((client) => start(client));
-  
-
-    function start(client){
-        app.listen(3000, function(){
-        console.log("Servidor Iniciado e escutando na porta 3000");
-    });
-
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + "/html/index.html");
-      })
-
-    app.get("/api", async function(req,res,next){
-        await client.sendText('55'+ req.query.celular + '@c.us', req.query.mensagem);
-        res.json(req.query);
+venom.create(
+  'API-Whatsapp', 
+    
+    (base64Qrimg, asciiQR, attempts) => {
+      console.log('Number of attempts to read the qrcode: ', attempts);
+      console.log('Terminal qrcode: ', asciiQR);
+      console.log('base64 image string qrcode: ', base64Qrimg);
+    },
+     (statusSession, session) => {
+      console.log('Status Session: ', statusSession); 
+      console.log('Session name: ', session);
+    },
+    {
+      browserArgs: ['--no-sandbox'],
+      disableWelcome: true,
     })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
 
-client.onStateChange((state) => {
-    console.log(state);
-    const conflits = [
-      venom.SocketState.CONFLICT,
-      venom.SocketState.UNPAIRED,
-      venom.SocketState.UNLAUNCHED,
-    ];
-    if (conflits.includes(state)) {
-      client.useHere();
-    }
+
+function start(client){
+  app.listen(3000, function(){
+    console.log("Servidor Iniciado e escutando na porta 3000");
+  });
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/html/index.html");
+  })
+  app.get("/api", async function(req,res,next){
+    await client.sendText('55'+ req.query.celular + '@c.us', req.query.mensagem);
+    res.json(req.query);
+  })
+  client.onStateChange((state) => {
+    console.log('State changed: ', state);
+    if ('CONFLICT'.includes(state)) client.useHere();
+    if ('UNPAIRED'.includes(state)) console.log('logout');
   });
 }
